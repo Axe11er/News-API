5365bbff71d528e20c9340680dc443c9e9e5141f
Alexander Naumov <Axeller@mail.ru>
2022-07-03 15:06:19 +0800

    refactor: migrate appLoader.js to typescript

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,6 +1,6 @@
 import Loader from './loader';
 
-class AppLoader extends Loader {
+abstract class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: '0f175c29ee1c407d99a35052c062de89', // получите свой ключ https://newsapi.org/
