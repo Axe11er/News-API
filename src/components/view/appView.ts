89ef09943f32c9e9b5b1446152f36e516bc85fde
Alexander Naumov <Axeller@mail.ru>
2022-07-03 15:10:04 +0800

    refactor: migrate appView.js to typescript

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,22 @@
+import INewsData from '../../interfaces/INewsData';
 import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: INewsData): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: INewsData): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
