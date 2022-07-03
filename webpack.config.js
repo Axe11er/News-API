f9ca627aba5b4f752cf3d89739f9c03225782f65
Alexander Naumov <Axeller@mail.ru>
2022-07-03 14:49:53 +0800

    feat: add typescript to project

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
+    devtool: 'inline-source-map',
     mode: 'development',
     module: {
         rules: [
@@ -12,14 +14,24 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.tsx?$/i,
+                use: 'ts-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
+        assetModuleFilename: `assets/images/[name][hash][ext][query]`,
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -27,6 +39,14 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, './src/assets/images/rs_school_js.svg'),
+                    to: path.resolve(__dirname, './dist/assets/images/'),
+                },
+            ],
+        }),
     ],
 };
 
